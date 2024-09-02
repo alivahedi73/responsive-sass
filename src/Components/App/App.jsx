c66924c48c1ce033095e9d3f349805f5ff35f867
Ali.vahedi <ali.bme73@gmail.com>
2024-09-02 21:31:51 +0430

    part_one

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+const App = () => {
+  return <>
+  <div className="row">
+    <div className="col-xs-12">
+    <div className="col-xs-6 sw"></div>
+    <div className="col-xs-4 sw1"></div>
+    <div className="col-xs-2 sw2"></div>
+    </div>
+  </div>
+  </>;
+};
+
+export default App;
